c7288cb3324f99e67f1081efdd629f0aeee97c8e
Gabriel Osuobiem <osuobiem@gmail.com>
2019-12-03 14:05:29 +0100

    added responsiveness manipulation in js

--- drople.js
+++ drople.js
@@ -1,40 +1,69 @@
-  document.querySelector('#file').addEventListener('change', e => {
-    upload(e.target)
-  })
+//  Manipulate Responsiveness
 
-  function upload(e) {
-    let file = e.files[0];
+let img = document.getElementsByClassName('drople-img')
+let dro = document.getElementById('drople')
+for (let i = 0; i < img.length; i++) {
+  if (screen.width >= 768) {
+    if (dro.offsetWidth <= screen.width / 2 && dro.offsetWidth > screen.width / 3) {
+      img[i].setAttribute('class', 'drople-img col-3')
+    } else if (dro.offsetWidth > screen.width / 2 && dro.offsetWidth <= screen.width) {
+      img[i].setAttribute('class', 'drople-img col-2')
+    } else if (dro.offsetWidth < screen.width / 2 && dro.offsetWidth >= screen.width / 5) {
+      img[i].setAttribute('class', 'drople-img col-4')
+    } else if (dro.offsetWidth < screen.width / 5) {
+      img[i].setAttribute('class', 'drople-img col-12')
+    }
+  } else {
+    if (dro.offsetWidth <= screen.width / 2) {
+      img[i].setAttribute('class', 'drople-img col-12')
+    } else {
+      img[i].setAttribute('class', 'drople-img col-6')
+    }
+  }
 
-    let fd = new FormData();
-    fd.append('file', file);
+  w = img[i].offsetWidth;
+  img[i].setAttribute('style', 'height: ' + w + 'px; ' + img[i].getAttribute('style'))
+}
 
-    let xhr = new XMLHttpRequest();
+// File upload 
 
-    xhr.open('PUT', 'http://localhost:1337/upload-portfile', true);
+document.querySelector('#file').addEventListener('change', e => {
+  upload(e.target)
+})
 
-    xhr.upload.addEventListener("loadstart", progress, false);
-    xhr.upload.addEventListener("progress", progress, false);
-    xhr.upload.addEventListener("load", progress, false);
-    xhr.upload.addEventListener("error", error, false);
+function upload(e) {
+  let file = e.files[0];
 
-    xhr.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        var response = this.responseText;
+  let fd = new FormData();
+  fd.append('file', file);
 
-        console.log(response)
-      } else if (this.status == 500) {
-        console.log('Server Error')
-      }
+  let xhr = new XMLHttpRequest();
 
+  xhr.open('PUT', 'http://localhost:1337/upload-portfile', true);
+
+  xhr.upload.addEventListener("loadstart", progress, false);
+  xhr.upload.addEventListener("progress", progress, false);
+  xhr.upload.addEventListener("load", progress, false);
+  xhr.upload.addEventListener("error", error, false);
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = this.responseText;
+
+      console.log(response)
+    } else if (this.status == 500) {
+      console.log('Server Error')
     }
 
-    xhr.send(fd);
   }
 
-  function progress(e) {
-    console.log(Math.round(e.loaded / e.total * 100))
-  }
+  xhr.send(fd);
+}
 
-  function error(e) {
-    console.log('Oops! something went wrong');
-  }
+function progress(e) {
+  console.log(Math.round(e.loaded / e.total * 100))
+}
+
+function error(e) {
+  console.log('Oops! something went wrong');
+}
